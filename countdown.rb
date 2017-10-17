67f8a8a72a64cd790655127a5f0b75c546340970
David Kuruvilla <dapawn@gmail.com>
2017-10-17 19:42:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(num)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  while num >= 0
+    num -= 1
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
